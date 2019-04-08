93ec98322b63eb9765a3b2aeb1a79fbed9f8738e
Jvrekk <you@example.com>
2019-04-08 16:18:30 +0200

    service, model, http get Api

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  constructor(private http: HttpClient) { }
+
+  getUsers(){
+    return this.http.get<User[]>(this.apiUrl);
+  }
+}
